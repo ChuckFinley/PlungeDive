737aa6653ce2e2d448bc5d3eb0f6c9fec30cd737
Max Czapanskiy <maxczapanskiy@gmail.com>
2016-06-08 12:05:26 -0700

    Initializing PlungeDive repo
    Contains TdrHelper.R for plotting events, TDR data, deployment metadata, and an Rproj.

---
+++ README.md
@@ -0,0 +1,2 @@
+# PlungeDive
+PlungeDive is a tool that uses machine learning to accurately, efficiently, and reproducibly identify diving behavior from depth data
---
+++ TdrHelper.R
@@ -0,0 +1,64 @@
+library(ggplot2)
+library(dplyr)
+
+# plot.fastlog returns a ggplot object depicting a portion of a CEFAS fastlog
+plot.fastlog <- function(deployid, eventid, start = NULL, span.sec = 5) {
+  # Read metadata. This returns two rows: deployment and recovery
+  metadata <- read.csv('CEFAS/metadata_all_GPS.csv') %>%
+    filter(Deploy_ID == deployid) %>%
+    arrange(Tagging_Event)
+  
+  # Assert exactly two rows found
+  if(nrow(metadata) == 0) stop(sprintf('No metadata for Deploy ID %i', deployid))
+  if(nrow(metadata) == 1) stop(sprintf('Recovery metadata not found for Deploy ID %i', deployid))
+  if(nrow(metadata) > 2) stop(sprintf('Multiple metadata records for Deploy ID %i', deployid))
+  
+  # Read TDR data and select records from input event
+  event <- read.csv(sprintf('CEFAS/%s.CSV', metadata$TDR_File[2]),
+                    stringsAsFactors = FALSE) %>%
+    filter(EventId == eventid) %>%
+    mutate(UTC = as.POSIXct(UTC,
+                            tz = 'UTC'))
+  
+  # Assert event exists
+  if(nrow(event) == 0) stop(sprintf('No event %i for Deploy ID %i', eventid, deployid))
+  
+  # Assert start falls within event
+  if(!between(start, min(event$UTC), max(event$UTC))) stop(sprintf('Start time %s falls outside of event %i for Deploy ID %i', start, eventid, deployid))
+  
+  ## Plot event
+  # Determine x-axis limits
+  event.xlim <- c(0, span.sec) + if(is.null(start)) event$UTC[1] else start
+  
+  # Filter event to within event.xlim
+  plot.data <- filter(event, 
+                      UTC >= event.xlim[1],
+                      UTC <= event.xlim[2])
+  
+  # Assert 
+  
+  # Date formatting function for x-axis
+  event.date.labels <- function(dates) format(dates, format = '%b %e %H:%M:%OS1')
+  
+  # Create event plot
+  # Date/time on x-axis, pressure on y-axis
+  event.plot <- ggplot(plot.data,
+                      aes(x = UTC,
+                          y = Pressure,
+                          group = 1)) +
+    # Add lines and points
+    geom_line() +
+    geom_point() +
+    # Set x-axis to display dates every second
+    # Fix xlim so events shorter than 1s still display
+    scale_x_datetime(date_breaks = '1 sec',
+                     labels = event.date.labels,
+                     limits = event.xlim) +
+    # Flip the y axis so it displays depth instead of pressure
+    scale_y_reverse() +
+    # Label axes and give plot a title
+    labs(x = 'Time',
+         y = 'Depth (m)',
+         title = sprintf('FieldID = %s\nDeployID = %i, EventID = %i', metadata$FieldID[1], deployid, eventid))
+}
+
